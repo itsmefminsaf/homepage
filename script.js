823b79b7030be6627b50a4412c7789d528280024
itsmefminsaf <itsmefminsaf@gmail.com>
2024-08-31 14:07:14 +0530

    added search functionality

--- script.js
+++ script.js
@@ -21,3 +21,36 @@ const showTime = () => {
 setInterval(() => {
   showTime();
 }, 1000);
+
+const searchEngine = document.getElementById("source");
+const searchTerm = document.getElementById("term");
+var engine = "gl";
+
+searchEngine.addEventListener("change", (e) => {
+  return (engine = e.target.value);
+});
+
+searchTerm.addEventListener("change", (e) => {
+  const search = e.target.value;
+  var link = "";
+  if (engine === "gl") {
+    console.log("gl", e.target.value);
+    link = `https://google.com/search=?${search.replace(" ", "+")}`;
+  } else if (engine === "github") {
+    console.log("github", search);
+    link = `https://github.com/search=?${search}`;
+  } else if (engine === "yt") {
+    console.log("yt", search);
+    link = `https://youtube.com/search=?${search}`;
+  } else if (engine === "wkp") {
+    console.log("wkp", search);
+    link = `https://wikipedia.com/search=?${search}`;
+  }
+
+  const a = document.createElement("a");
+  a.href = link;
+  a.target == "_blank";
+  a.click();
+  a.remove();
+  e.target.value = "";
+});
